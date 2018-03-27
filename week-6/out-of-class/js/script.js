c53ea6a7bb912a5feb4841ac946ef975a01f4a39
deal <deal@simmons.edu>
2018-03-26 20:57:59 -0400

    dailyHours() assignment not done

--- week-6/out-of-class/js/script.js
+++ week-6/out-of-class/js/script.js
@@ -4,3 +4,35 @@ function renderOutput(output) {
     let output_element = document.getElementById("output");
     output_element.innerHTML += "<div>" + output + "</div>"
 }
+
+//set variables
+let hours = [
+    "Monday: 9a - 9p",
+    "Tuesday: 9a - 9p",
+    "Wednesday: 9a - 9p",
+    "Thursday: 9a - 9p",
+    "Friday: 9a - 5p",
+    "Saturday: 9a - 5p",
+    "Sunday: closed"
+];
+
+let space = ' '
+
+    function displayOneDay(one_days_hours) {
+        renderOutput(one_days_hours)
+    
+
+    let output = one_days_hours = one_days_hours + " - ";
+
+    displayOneDay("Monday: 9a - 9p");
+
+    }
+
+
+
+//split the array
+//function splitString () {
+//let days_of_week = hours.split(", ");
+//console.log(days_of_week[1], days_of_week[0]);
+//}
+
